b600d604d56a4bb4bb006e222fec03d984805cf8
Niko Ehrenfeuchter <mail@he1ix.org>
2014-03-05 16:47:33 +0100

    Stub module for printing metadata.
    Basically a slightly modified version of the original training example script,
    wrapped into a function for being imported into the IPython shell in OMERO.
    
    Original source:
    https://github.com/openmicroscopy/openmicroscopy/blob/develop/examples/Training/python/Metadata.py

---
+++ Metadata.py
@@ -0,0 +1,82 @@
+def print_metadata(conn, imgid):
+    image = conn.getObject('Image', imgid)
+    print "*", image.getDataset().getParent().getName()
+    print " \_", image.getDataset().getName()
+    print "   \_", image.getName() , "\n"
+
+    # Instrument
+    # ==========
+    instrument = image.getInstrument()
+    if instrument is not None:
+        # Instrument contains links to all filters,
+        # objectives etc (not just the ones used for this image)
+        if instrument.getMicroscope() is not None:
+            print "Instrument:"
+            microscope = instrument.getMicroscope()
+            print "  Model:", microscope.getModel(), "Type:", microscope.getType() and microscope.getType().getValue()
+
+
+    # Load ObjectiveSettings
+    # ======================
+    if image.getObjectiveSettings():
+        objSet = image.getObjectiveSettings()
+        print "Objective Settings:"
+        print("  Correction Collar:", objSet.getCorrectionCollar(), "Medium:",
+              objSet.getMedium(), "Refractive Index:",
+              objSet.getRefractiveIndex())
+        if objSet.getObjective():
+            obj = objSet.getObjective()
+            print "Objective:"
+            print "  Model:", obj.getModel(), "Nominal Mag:", obj.getNominalMagnification(), "Calibrated Mag:", obj.getCalibratedMagnification()
+            print "  LensNA:", obj.getLensNA(), "Immersion",
+            print obj.getImmersion() and obj.getImmersion().getValue(), "Correction:", obj.getCorrection() and obj.getCorrection().getValue()
+            print "  Working Distance:", obj.getWorkingDistance()
+
+    # Load Channels, LogicalChannels, and LightSourceSettings
+    # =================================================================
+    for ch in image.getChannels():
+        print "Channel: ", ch.getLabel()
+        logicalChannel = ch.getLogicalChannel()
+
+        lightPath = logicalChannel.getLightPath()
+        if lightPath is not None:
+            lightPathDichroic = lightPath.getDichroic()
+            print "  Dichroic:"
+            print "    Model:", lightPathDichroic.getModel()
+            print "  Emission Filters:"
+            for f in lightPath.getEmissionFilters():
+                print "    Model:", f.getModel(),
+                print "    Type:", f.getType() and f.getType().getValue(),
+                tr = f.getTransmittanceRange()
+                print "    Transmittance range:", tr.getCutIn(), "-", tr.getCutOut()
+            print "  Excitation Filters:"
+            for f in lightPath.getExcitationFilters():
+                print "    Model:", f.getModel(),
+                print "    Type:", f.getType() and f.getType().getValue(),
+                tr = f.getTransmittanceRange()
+                print "    Transmittance range:", tr.getCutIn(), "-", tr.getCutOut()
+
+        if logicalChannel.getDetectorSettings()._obj is not None:
+            print "  Detector Settings:"
+            dset = logicalChannel.getDetectorSettings()
+            print "    Voltage:", dset.getVoltage(), "Gain:", dset.getGain(), "Offset:", dset.getOffsetValue(),
+            print "Readout rate:", dset.getReadOutRate(),
+            print "Binning:", dset.getBinning()._obj is not None and dset.getBinning().getValue()
+            if logicalChannel.getDetectorSettings().getDetector():
+                print "  Detector:"
+                det = logicalChannel.getDetectorSettings().getDetector()
+                print "    Model:", det.getModel(), "Gain:", det.getGain(), "Voltage:", det.getVoltage(), "Offset:", det.getOffsetValue()
+
+        lightSourceSettings = logicalChannel.getLightSourceSettings()
+        if lightSourceSettings is not None and lightSourceSettings._obj is not None:
+            print "  Light Source:"
+            if lightSourceSettings.getLightSource() is not None:
+                ls = lightSourceSettings.getLightSource()
+                print "    Model:", ls.getModel(), "Manufacturer:", ls.getManufacturer(), "Power:", ls.getPower()
+                # TODO: Check whether this is Arc etc.
+                try:
+                    wl = ls.getWavelength()
+                    print "    Laser Wavelength:", wl
+                except AttributeError:
+                    # this is not a laser
+                    pass
