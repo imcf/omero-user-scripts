3141a08de0200d8f22a43a3c1a4c767b5680088f
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2015-12-16 16:50:14 +0100

    Adapt cron_runner to new Python script.

--- cron_runner.sh
+++ cron_runner.sh
@@ -1,34 +1,48 @@
 #!/bin/bash
 
+LOGDIR="$HOME/OMERO.server/var/log/cron"
+REPOBASE="/export/omero/OMERO/ManagedRepository"
+
 export PYTHONPATH=$PYTHONPATH:$HOME/OMERO.server/lib/python
 
 cd $(dirname $0)
 
 if [ -z "$1" ] ; then
-    echo "ERROR: file with list of usernames required!"
+    echo "ERROR: comma-separated list of usernames required!"
     exit 1
 else
-    USERLIST="$1"
+    USERS="$1"
+fi
+
+if ! [ -d "$LOGDIR" ] ; then
+    set -e
+    mkdir -v "$LOGDIR"
+    set +e
 fi
 
 # extglob is required for the pattern matching in the OMERO_EXPORT line below
 shopt -s extglob
 
-REPOBASE="/export/omero/OMERO/ManagedRepository"
-TREE="omero_hierarchy/tree"
-
-for OMERO_USERNAME in $(cat "$USERLIST") ; do
-    echo "========================== $OMERO_USERNAME =========================="
-    OMERO_EXPORT=$(echo $REPOBASE/${OMERO_USERNAME}_+([[:digit:]])/omero_hierarchy)
-    if [ -d "$OMERO_EXPORT/tree" ] ; then
-	mv "$OMERO_EXPORT/tree" "$OMERO_EXPORT/tree_old"
+for OMERO_USERNAME in ${USERS//,/ } ; do
+    TREE=$(echo $REPOBASE/${OMERO_USERNAME}_+([[:digit:]])/omero_hierarchy/tree)
+    if [ -d "$TREE" ] ; then
+	echo "Temporarily moving old export tree:"
+        mv -v "$TREE" "${TREE}_old"
     fi
-    cat localconfig_template.py | sed "s,OMERO_USERNAME,${OMERO_USERNAME}," > localconfig.py
-    TSTART=$(date +%s)
-    LOGFILE="$OMERO_EXPORT/.cron_run_$TSTART.log"
-    echo "Starting export of OMERO hierarchy, logging to '$LOGFILE'."
-    ./export_treestructure.py > "$LOGFILE" 2>&1
-    TDELTA=$(echo "$(date +%s) - $TSTART" | bc -l)
-    echo "Export of OMERO hierarchy took $TDELTA seconds."
-    rm -rf "$OMERO_EXPORT/tree_old"
+done
+
+TSTART=$(date +%s)
+LOGFILE="$LOGDIR/export_treestructure-$TSTART.log"
+echo "Starting export of OMERO hierarchy, logging to '$LOGFILE'."
+# ./export_treestructure.py --user $USERS -vv 2>&1 | tee log
+./export_treestructure.py --user $USERS -vv > "$LOGFILE" 2>&1
+TDELTA=$(echo "$(date +%s) - $TSTART" | bc -l)
+echo "Export of OMERO hierarchy took $TDELTA seconds."
+
+for OMERO_USERNAME in ${USERS//,/ } ; do
+    TREE=$(echo $REPOBASE/${OMERO_USERNAME}_+([[:digit:]])/omero_hierarchy/tree)
+    if [ -d "$TREE" ] ; then
+	echo "Removing temporary backupg of old export tree: ${TREE}_old"
+        rm -rf "${TREE}_old"
+    fi
 done
